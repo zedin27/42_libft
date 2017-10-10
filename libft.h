0d81e2216bb1b11a002ca3352f8e34d878429f00
Zeid Tisnes <ztisneshamzeh@csumb.edu>
2017-10-10 05:26:35 -0700

    Update libft.h

--- libft.h
+++ libft.h
@@ -17,9 +17,6 @@
 # include <stdlib.h>
 # include <string.h>
 
-/*
-** List structure
-*/
 typedef struct		s_list
 {
 	void			*content;
@@ -33,10 +30,6 @@ void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstadd(t_list **alst, t_list *new);
 void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
-
-/*
-** Memory Manipulation
-*/
 void				ft_memdel(void **ap);
 void				*ft_memalloc(size_t size);
 void				*ft_memset(void *ptr, int value, size_t num);
@@ -46,10 +39,6 @@ void				*ft_memchr(const void *str, int c, size_t n);
 void				*ft_memccpy(void *dst, const void *src, int c, size_t n);
 int					ft_memcmp(const void *str1, const void *str2, size_t n);
 void				ft_bzero(void *s, size_t n);
-
-/*
-** String manipulation
-*/
 char				*ft_strcpy(char *dest, const char *src);
 char				*ft_strchr(const char *s, int c);
 char				*ft_strdup(const char *str);
@@ -71,17 +60,12 @@ char				*ft_strmapi(char const *s, char(*f)(unsigned int, char));
 void				ft_striter(char *s, void(*f)(char *));
 void				ft_striteri(char *s, void(*f)(unsigned int, char *));
 char				*ft_strrchr(const char *s, int c);
-char				*ft_strnstr(const char *big, const char *little, \
-								size_t len);
+char				*ft_strnstr(const char *big, const char *little, \								size_t len);
 int					ft_strcmp(const char *s1, const char *s2);
 char				*ft_strnew(size_t size);
 char				**ft_strsplit(char const *s, char c);
 char				*ft_strtrim(char const *string);
 char				*ft_strtok(char *str, const char delim);
-
-/*
-** Misc.
-*/
 int					ft_atoi(char *str);
 char				*ft_itoa_base(int n, int base);
 char				*ft_itoa(int num);

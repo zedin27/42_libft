14c0d04455afc252ca4ebe05244d8ae740b80eb4
zedin27 <ztisneshamzeh@csumb.edu>
2018-02-03 19:33:48 -0800

    new functions from grimly

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: ztisnes <ztisnes@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2017/09/20 18:13:08 by ztisnes           #+#    #+#              #
-#    Updated: 2018/01/10 23:25:11 by ztisnes          ###   ########.fr        #
+#    Updated: 2018/02/03 19:29:14 by ztisnes          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -28,7 +28,8 @@ ft_strdup.c ft_strequ.c ft_striter.c ft_striteri.c ft_strjoin.c ft_strlcat.c \
 ft_strlen.c ft_strmap.c ft_strmapi.c ft_strncat.c ft_strncmp.c ft_strncpy.c \
 ft_strnequ.c ft_strnew.c ft_strnstr.c ft_strrchr.c ft_strsplit.c ft_strstr.c \
 ft_strsub.c ft_strtok.c ft_strtrim.c ft_tolower.c ft_toupper.c \
-ft_wordcount.c ft_wordlength.c expression_tree.c queue.c stack.c
+ft_wordcount.c ft_wordlength.c expression_tree.c queue.c stack.c \
+ft_abs.c ft_realloc.c ft_manhattan_heuristic.c 
 
 OBJS    = $(SRCS:.c=.o)
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ztisnes <ztisnes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/21 21:47:24 by ztisnes           #+#    #+#             */
-/*   Updated: 2018/01/10 23:35:14 by ztisnes          ###   ########.fr       */
+/*   Updated: 2018/02/03 19:33:21 by ztisnes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,12 +45,17 @@ typedef struct		s_btree
 	struct s_btree	*right;
 }					t_btree;
 
+int					ft_manhattan_heuristic(t_list *a, t_list *b);
+int					ft_abs(int value);
+{
+
 t_list				*ft_lstnew(void const *content, size_t content_size);
 void				ft_lstdelone(t_list **alst, void (*del)(void*, size_t));
 void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstadd(t_list **alst, t_list *new);
 void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
+
 void				ft_memdel(void **ap);
 void				*ft_memalloc(size_t size);
 void				*ft_memset(void *ptr, int value, size_t num);
@@ -60,6 +65,8 @@ void				*ft_memchr(const void *str, int c, size_t n);
 void				*ft_memccpy(void *dst, const void *src, int c, size_t n);
 int					ft_memcmp(const void *str1, const void *str2, size_t n);
 void				ft_bzero(void *s, size_t n);
+void				*ft_realloc(void *ptr, size_t size)
+
 char				*ft_strcpy(char *dest, const char *src);
 char				*ft_strchr(const char *s, int c);
 char				*ft_strdup(const char *str);
@@ -91,6 +98,7 @@ char				*ft_strtok(char *str, const char delim);
 int					ft_atoi(char *str);
 char				*ft_itoa_base(int n, int base);
 char				*ft_itoa(int num);
+
 void				ft_putchar(char c);
 void				ft_putchar_fd(char c, int fd);
 void				ft_putendl(char const *s);

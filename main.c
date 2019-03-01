2be00c7d9cd4959c34c79ee6762b8a4d8310ef3c
zedin27 <ztisneshamzeh@csumb.edu>
2019-03-01 15:06:52 -0800

    testing queue properly

--- main.c
+++ main.c
@@ -7,19 +7,26 @@ int main(void)
 	t_queue *q = init_queue();
 
 	printf("Empty? %d\n", is_empty(q));
-	printf("Enqueueing 'Hello'\n");
-	enqueue(q, "Hello");
+	// printf("Enqueueing 'Hello'\n");
+	enqueue(q, "3");
+	// printf("Empty? %d\n", is_empty(q));
+	// printf("Peeking: %s\n", peek_queue(q));
+	// printf("Enqueueing 'World'\n");
+	enqueue(q, "2");
+	// printf("Empty? %d\n", is_empty(q));
+	// printf("Peeking: %s\n", peek_queue(q));
+	// printf("Enqueueing ':)'\n");
+	enqueue(q, "1");
+	enqueue(q, "a");
+	enqueue(q, "b");
+	enqueue(q, "c");
+	enqueue(q, "d");
+	enqueue(q, "e");
+	enqueue(q, "f");
 	printf("Empty? %d\n", is_empty(q));
 	printf("Peeking: %s\n", peek_queue(q));
-	printf("Enqueueing 'World'\n");
-	enqueue(q, "World");
-	printf("Empty? %d\n", is_empty(q));
-	printf("Peeking: %s\n", peek_queue(q));
-	printf("Enqueueing ':)'\n");
-	enqueue(q, ":)");
-	printf("Empty? %d\n", is_empty(q));
-	printf("Peeking: %s\n", peek_queue(q));
-	print_queue(q);
+
+	ft_print_queue(q);
 
 	return 0;
 }
